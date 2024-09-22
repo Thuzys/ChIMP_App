97b7ae2e421edf071e11aa755b37ef8d83d7915b
Brgm37 <brgm37@gmail.com>
2024-09-22 09:51:12 +0100

    - duplicated email property removed - imageId property added to data class about in order to associate images to about instances

--- app/src/main/java/com/example/chimp/model/about/About.kt
+++ app/src/main/java/com/example/chimp/model/about/About.kt
@@ -7,12 +7,14 @@ package com.example.chimp.model.about
  * @param email The email of the user.
  * @param socialMedia The social media information of the user.
  * @param bio The biography of the user.
+ * @param imageId Resources object to query the image file from.
  */
 data class About(
     val name: String,
     val email: Email,
     val socialMedia: SocialMedia? = null,
     val bio: String? = null,
+    val imageId: String? = null
 ) {
     init {
         require(name.isNotBlank()) { "Name must not be blank." }
--- app/src/main/java/com/example/chimp/model/about/SocialMedia.kt
+++ app/src/main/java/com/example/chimp/model/about/SocialMedia.kt
@@ -5,13 +5,11 @@ import java.net.URL
 /**
  * Data class that represents the contact information of the user.
  *
- * @param email The email of the user.
  * @param gitHub The GitHub URL of the user.
  * @param linkedIn The LinkedIn URL of the user.
 
  */
 data class SocialMedia(
-    val email: Email,
     val gitHub: URL? = null,
     val linkedIn: URL? = null,
 ) {
