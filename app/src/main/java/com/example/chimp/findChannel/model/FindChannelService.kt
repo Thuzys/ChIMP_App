f2f238d69ad5682b9f7bac0c82304e1d14e03af3
Brgm37 <brgm37@gmail.com>
2024-11-01 23:20:34 +0000

    FindChannelService first implementation

--- app/src/main/java/com/example/chimp/findChannel/model/FindChannelService.kt
+++ app/src/main/java/com/example/chimp/findChannel/model/FindChannelService.kt
@@ -1,4 +1,11 @@
 package com.example.chimp.findChannel.model
 
+/**
+ * Interface that defines the service used in FindChannelViewModel.
+ */
 interface FindChannelService {
+    /**
+     * Join a channel.
+     */
+    fun joinChannel(channelId: String, userId: String, callback: (FindChannelErrors?) -> Unit)
 }
