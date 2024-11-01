65216ef4800e2e6175b20922e42b382d2a237aba
Brgm37 <brgm37@gmail.com>
2024-11-01 23:15:12 +0000

    -FindChannelViewModel implementation

---
+++ app/src/main/java/com/example/chimp/findChannel/screen/ChIMPFindChannelScreen.kt
@@ -0,0 +1,13 @@
+package com.example.chimp.findChannel.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.chimp.findChannel.viewModel.FindChannelViewModel
+
+@Composable
+fun ChIMPFindChannelScreen(
+    modifier: Modifier = Modifier,
+    viewModel: FindChannelViewModel,
+) {
+
+}
--- app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelViewModel.kt
+++ app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelViewModel.kt
@@ -1,2 +1,38 @@
 package com.example.chimp.findChannel.viewModel
 
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chimp.findChannel.model.FindChannelErrors
+import com.example.chimp.findChannel.model.FindChannelItem
+import com.example.chimp.findChannel.viewModel.state.FindChannelScreenState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class FindChannelViewModel: ViewModel() {
+    val scrollState = LazyListState()
+    private val items = MutableStateFlow<List<FindChannelItem>>(emptyList())
+    val chatItems: StateFlow<List<FindChannelItem>> get() = items
+
+    var state: FindChannelScreenState = FindChannelScreenState.Loading
+        private set
+
+    fun updateChatItems(newItems: List<FindChannelItem>) {
+        viewModelScope.launch {
+            items.value = newItems
+        }
+    }
+
+    fun joinChannel(channelId: UInt) {
+        state = FindChannelScreenState.Joining(channelId)
+    }
+
+    fun channelJoined(channelId: UInt) {
+        state = FindChannelScreenState.Joined(channelId)
+    }
+
+    fun channelJoinError(error: String) {
+        state = FindChannelScreenState.Error(FindChannelErrors.JoinError(error))
+    }
+}
