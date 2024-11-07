d78708c5f8e6201510fc8bda44b0169f52e43404
Brgm37 <brgm37@gmail.com>
2024-11-07 02:39:16 +0000

    FindChannelViewModel was implemented

--- app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelViewModel.kt
+++ app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelViewModel.kt
@@ -1,38 +1,88 @@
 package com.example.chimp.findChannel.viewModel
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.chimp.findChannel.model.FindChannelErrors
+import com.example.chimp.about.viewModel.state.AboutScreenState
+import com.example.chimp.chats.model.channel.ChannelName
+import com.example.chimp.either.Failure
+import com.example.chimp.either.Success
 import com.example.chimp.findChannel.model.FindChannelItem
+import com.example.chimp.findChannel.model.FindChannelService
+import com.example.chimp.findChannel.viewModel.state.FindChannel
 import com.example.chimp.findChannel.viewModel.state.FindChannelScreenState
+import com.example.chimp.login.viewModel.state.LoginScreenState
+import com.example.chimp.model.errors.ResponseErrors
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
-class FindChannelViewModel: ViewModel() {
-    val scrollState = LazyListState()
-    private val items = MutableStateFlow<List<FindChannelItem>>(emptyList())
-    val chatItems: StateFlow<List<FindChannelItem>> get() = items
-
-    var state: FindChannelScreenState = FindChannelScreenState.Loading
+class FindChannelViewModel(
+    private val service: FindChannelService
+) : ViewModel() {
+    var state: FindChannelScreenState by mutableStateOf(FindChannelScreenState.Loading)
         private set
 
-    fun updateChatItems(newItems: List<FindChannelItem>) {
+    fun joinChannel(channelId: UInt) {
+        val curr = state
+        if (curr !is FindChannel.FindChannelIdle) return
+        state = FindChannelScreenState.Loading
         viewModelScope.launch {
-            items.value = newItems
+            state = when (val result = service.joinChannel(channelId)) {
+                is Success -> FindChannelScreenState.Joined(channelId)
+                is Failure -> FindChannelScreenState.Error(result.value)
+            }
         }
     }
 
-    fun joinChannel(channelId: UInt) {
-        state = FindChannelScreenState.Joining(channelId)
+    fun findChannel(channelName: String) {
+        val curr = state
+        if (curr !is FindChannel.FindChannelIdle) return
+        state = FindChannelScreenState.Loading
+        viewModelScope.launch {
+            state = when (val result = service.findChannel(ChannelName(channelName))) {
+                is Success -> FindChannelScreenState.Success(listOf(result.value))
+                is Failure -> FindChannelScreenState.Error(result.value)
+            }
+        }
     }
 
-    fun channelJoined(channelId: UInt) {
-        state = FindChannelScreenState.Joined(channelId)
+    fun getChannels(offset: UInt?, limit: UInt?) {
+        val curr = state
+        if (curr !is FindChannel.FindChannelIdle) return
+        state = FindChannelScreenState.Loading
+        viewModelScope.launch {
+            state = when (val result = service.getChannels(offset, limit)) {
+                is Success -> FindChannelScreenState.Success(result.value.toList())
+                is Failure -> FindChannelScreenState.Error(result.value)
+            }
+        }
     }
 
-    fun channelJoinError(error: String) {
-        state = FindChannelScreenState.Error(FindChannelErrors.JoinError(error))
+    fun updateSearchChannelInput(searchChannelInput: String) {
+        val curr = state
+        if (curr is FindChannel.FindChannelIdle) {
+            state = curr.updateSearchChannelInput(searchChannelInput)
+        }
+    }
+
+    fun updatePublicChannels(publicChannels: List<FindChannelItem>) {
+        val curr = state
+        if (curr is FindChannel.FindChannelIdle) {
+            state = curr.updatePublicChannels(publicChannels)
+        }
+    }
+
+    fun toFindChannel() {
+        if (state !is FindChannel) {
+            state = FindChannel.FindChannelIdle("", null)
+        }
     }
 }
