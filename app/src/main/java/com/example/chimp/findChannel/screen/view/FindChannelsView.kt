8ae7bacfb708a9bbb3b0f9d4e7f1c41ea4efb5dc
Brgm37 <brgm37@gmail.com>
2024-11-07 02:38:53 +0000

    FindChannelsView was modified to support changes

--- app/src/main/java/com/example/chimp/findChannel/screen/view/FindChannelsView.kt
+++ app/src/main/java/com/example/chimp/findChannel/screen/view/FindChannelsView.kt
@@ -1,56 +1,82 @@
 package com.example.chimp.findChannel.screen.view
 
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.chimp.R
 import com.example.chimp.chats.model.channel.ChannelName
 import com.example.chimp.findChannel.model.FindChannelItem
 import com.example.chimp.findChannel.screen.composable.ChatItemRow
-import com.example.chimp.findChannel.viewModel.FindChannelViewModel
+import com.example.chimp.findChannel.screen.composable.SearchBar
+import com.example.chimp.findChannel.viewModel.state.FindChannel
+import com.example.chimp.findChannel.viewModel.state.FindChannelScreenState
 
 @Composable
-fun ChatList(
+fun FindChannelView(
     modifier: Modifier = Modifier,
-    vm: FindChannelViewModel
+    vm: FindChannelScreenState,
+    onJoin: (UInt) -> Unit = {},
+    onValueChange: (String) -> Unit = {},
+    onSearch: (String) -> Unit = {}
 ) {
-    val chatItems by vm.chatItems.collectAsState()
-
-    LazyColumn(modifier = modifier.fillMaxSize().scrollable(state = vm.scrollState, orientation = Orientation.Vertical)) {
-        items(chatItems) { chatItem ->
-            ChatItemRow(chatItem)
-            HorizontalDivider(thickness = 0.5.dp, color = Color.LightGray)
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        SearchBar(
+            modifier = modifier,
+            value = vm.searchChannelInput,
+            onValueChange = onValueChange,
+            onSearch = { onSearch(vm.searchChannelInput) },
+        )
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            val channels = vm.publicChannels
+            channels?.forEach { chatItem ->
+                ChatItemRow(
+                    chatItem = chatItem,
+                    onJoin = { onJoin(chatItem.cId) }
+                )
+                Spacer(Modifier.width(5.dp))
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ChatListPreview() {
-    val vm = FindChannelViewModel().apply {
-        val list: MutableList<FindChannelItem> = mutableListOf()
-        repeat (20) {
-            list.add(
-                FindChannelItem(
-                    cId = it.toUInt(),
-                    icon = R.drawable.github_mark,
-                    name = ChannelName("Channel $it"),
-                )
+private fun ChatListPreview() {
+    val channels = mutableListOf<FindChannelItem>()
+    repeat(50) {
+        channels.add(
+            FindChannelItem(
+                cId = it.toUInt(),
+                name = ChannelName("Channel $it"),
+                icon = R.drawable.github_mark,
             )
-        }
-        updateChatItems(list)
+        )
     }
-    ChatList(
-        vm = vm
+    val vm =
+        FindChannel.FindChannelIdle(
+            searchChannelInput = "",
+            publicChannels = channels,
+
+    )
+    FindChannelView(
+        vm = vm,
+        onJoin = { }
     )
 }
