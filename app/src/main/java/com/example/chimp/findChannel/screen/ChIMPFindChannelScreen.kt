9693048a6f60de827ae8f6f785e7ec238c520594
Brgm37 <brgm37@gmail.com>
2024-11-07 02:38:08 +0000

    ChIMPFindChannelScreen was implemented

--- app/src/main/java/com/example/chimp/findChannel/screen/ChIMPFindChannelScreen.kt
+++ app/src/main/java/com/example/chimp/findChannel/screen/ChIMPFindChannelScreen.kt
@@ -1,13 +1,65 @@
 package com.example.chimp.findChannel.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.chimp.findChannel.screen.view.FindChannelView
 import com.example.chimp.findChannel.viewModel.FindChannelViewModel
+import com.example.chimp.findChannel.viewModel.state.FindChannel
+import com.example.chimp.findChannel.viewModel.state.FindChannelScreenState
+import com.example.chimp.login.screen.view.ErrorView
+import com.example.chimp.login.screen.view.LoadingView
 
 @Composable
 fun ChIMPFindChannelScreen(
     modifier: Modifier = Modifier,
     viewModel: FindChannelViewModel,
+    onJoinChannel: () -> Unit,
 ) {
+    when(val curr = viewModel.state) {
+        is FindChannel -> {
+            FindChannelBase(modifier, curr, viewModel)
+        }
+        is FindChannelScreenState.Error -> {
+            ErrorView(
+                modifier = modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                error = curr.error,
+                tryAgain = viewModel::toFindChannel,
+            )
+        }
+        is FindChannelScreenState.Joined -> {
+            onJoinChannel()
+        }
+        is FindChannelScreenState.Loading -> {
+            LoadingView(
+                modifier = modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+            )
+        }
+        is FindChannelScreenState.Success -> {
+            FindChannelBase(modifier, curr, viewModel)
+        }
+    }
+}
 
+@Composable
+private fun FindChannelBase(
+    modifier: Modifier,
+    curr: FindChannelScreenState,
+    viewModel: FindChannelViewModel
+) {
+    FindChannelView(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        vm = curr,
+        onValueChange = viewModel::updateSearchChannelInput,
+        onJoin = viewModel::joinChannel,
+        onSearch = viewModel::findChannel,
+    )
 }
