bd299f681b4acfee710ad41126c86e0a869d3404
Brgm37 <brgm37@gmail.com>
2024-11-07 02:34:06 +0000

    FindChannelVMFactory implemented

--- app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelVMFactory.kt
+++ app/src/main/java/com/example/chimp/findChannel/viewModel/FindChannelVMFactory.kt
@@ -1,4 +1,18 @@
 package com.example.chimp.findChannel.viewModel
 
-class FindChannelVMFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.chimp.findChannel.model.FindChannelService
+
+/**
+ * The factory for the [FindChannelViewModel].
+ *
+ * @property service the service used in the ViewModel context.
+ */
+@Suppress("UNCHECKED_CAST")
+class FindChannelVMFactory(private val service: FindChannelService) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return FindChannelViewModel(service) as T
+    }
+
 }
