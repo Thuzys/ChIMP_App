fc19d25adaf40af611908aa97b7400834a101f02
Brgm37 <brgm37@gmail.com>
2024-11-07 02:33:42 +0000

    DummyFindChannelService implementation and dependency findChannelService addded to dependecy container

--- app/src/main/java/com/example/chimp/services/dummy/DummyFindChannelService.kt
+++ app/src/main/java/com/example/chimp/services/dummy/DummyFindChannelService.kt
@@ -1,4 +1,25 @@
 package com.example.chimp.services.dummy
 
-class DummyFindChannelService {
+import com.example.chimp.chats.model.channel.ChannelName
+import com.example.chimp.either.Either
+import com.example.chimp.findChannel.model.FindChannelItem
+import com.example.chimp.findChannel.model.FindChannelService
+import com.example.chimp.model.errors.ResponseErrors
+import kotlinx.coroutines.flow.Flow
+
+class DummyFindChannelService: FindChannelService {
+    override suspend fun joinChannel(channelId: UInt): Either<ResponseErrors, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun findChannel(channelName: ChannelName): Either<ResponseErrors, FindChannelItem> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getChannels(
+        offset: UInt?,
+        limit: UInt?
+    ): Either<ResponseErrors, Flow<FindChannelItem>> {
+        TODO("Not yet implemented")
+    }
 }
