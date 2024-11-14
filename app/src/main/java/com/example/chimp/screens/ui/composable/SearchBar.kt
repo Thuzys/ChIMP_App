5bcda4dbdcfa0e021eaad19b3f00d46dda13674b
Brgm37 <brgm37@gmail.com>
2024-11-14 09:02:48 +0000

    minor change

--- app/src/main/java/com/example/chimp/screens/ui/composable/SearchBar.kt
+++ app/src/main/java/com/example/chimp/screens/ui/composable/SearchBar.kt
@@ -1,6 +1,5 @@
 package com.example.chimp.screens.ui.composable
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
