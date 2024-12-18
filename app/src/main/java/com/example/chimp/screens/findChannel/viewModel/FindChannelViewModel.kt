435d5b6c5f248129dc1212ff30971c65f900ad64
Brgm37 <brgm37@gmail.com>
2024-12-18 21:40:09 +0000

    - view model refactor - userRepository added to vm Factory

--- app/src/main/java/com/example/chimp/screens/findChannel/viewModel/FindChannelViewModel.kt
+++ app/src/main/java/com/example/chimp/screens/findChannel/viewModel/FindChannelViewModel.kt
@@ -1,81 +1,108 @@
 package com.example.chimp.screens.findChannel.viewModel
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.chimp.models.channel.ChannelName
 import com.example.chimp.models.either.Failure
 import com.example.chimp.models.either.Success
 import com.example.chimp.models.errors.ResponseError
+import com.example.chimp.models.repository.UserInfoRepository
 import com.example.chimp.screens.findChannel.model.FindChannelService
 import com.example.chimp.screens.findChannel.viewModel.state.FindChannelScreenState
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
+private const val DEBOUNCE_TIME = 500L
+
+/**
+ * ViewModel for the Find Channel screen.
+ *
+ * This ViewModel is responsible for managing the state of the Find Channel screen.
+ *
+ * @property service the service used in the ViewModel context.
+ */
+
+@OptIn(FlowPreview::class)
 class FindChannelViewModel(
-    private val service: FindChannelService
+    private val service: FindChannelService,
+    private val userInfoRepository: UserInfoRepository,
+    initialState: FindChannelScreenState = FindChannelScreenState.Init
 ) : ViewModel() {
-    var state: FindChannelScreenState by mutableStateOf(FindChannelScreenState.Loading)
-        private set
+    private val _state = MutableStateFlow(initialState)
+    private val _searchText = MutableStateFlow("")
+    val state: StateFlow<FindChannelScreenState> = _state
+
+    init {
+        viewModelScope.launch {
+            _searchText
+                .debounce(DEBOUNCE_TIME)
+                .map { searchChannelInput -> searchChannelInput }
+                .collect { searchChannelInput ->
+                    val curr = state.value
+                    if (curr !is FindChannelScreenState.Idle) return@collect
+                    _state.emit(FindChannelScreenState.Loading)
+                    when (val result = service.findChannelsByPartialName(ChannelName(searchChannelInput))) {
+                        is Success -> _state.emit(FindChannelScreenState.Idle(_searchText.value, result.value))
+
+                        is Failure -> _state.emit(FindChannelScreenState.Error(
+                            ResponseError(
+                                result.value.cause,
+                                result.value.urlInfo
+                            )
+                        ))
+                    }
+                }
+        }
+    }
 
     fun joinChannel(channelId: UInt) {
-        val curr = state
-        if (curr !is FindChannelScreenState.Idle) return
-        state = FindChannelScreenState.Loading
         viewModelScope.launch {
-            state = when (val result = service.joinChannel(channelId)) {
-                is Success -> FindChannelScreenState.Idle(publicChannels = curr.publicChannels)
-
-                is Failure ->
-                    FindChannelScreenState.Error(
+            val curr = state.value
+            if (curr !is FindChannelScreenState.Idle) return@launch
+            _state.emit(FindChannelScreenState.Loading)
+            _state.emit(
+                when (val result = service.joinChannel(channelId)) {
+                    is Success -> FindChannelScreenState.Init
+                    is Failure -> FindChannelScreenState.Error(
                         ResponseError(
                             result.value.cause,
                             result.value.urlInfo
                         )
                     )
-            }
+                }
+            )
         }
     }
 
-    fun findChannel(channelName: String) {
-        val curr = state
-        if (curr !is FindChannelScreenState.Idle) return
-        state = FindChannelScreenState.Loading
-        viewModelScope.launch {
-            state = when (val result = service.findChannelsByPartialName(ChannelName(channelName))) {
-                is Success -> FindChannelScreenState.Idle(
-                    publicChannels = result.value
-                )
-
-                is Failure -> FindChannelScreenState.Error(
-                    ResponseError(
-                        result.value.cause,
-                        result.value.urlInfo
-                    )
-                )
-            }
-        }
-    }
 
     fun getChannels(offset: UInt?, limit: UInt?) {
-        val curr = state
-        if (curr !is FindChannelScreenState.Idle) return
-        state = FindChannelScreenState.Loading
         viewModelScope.launch {
-            state = when (val result = service.getChannels(offset, limit)) {
-                is Success ->
-                    FindChannelScreenState.Idle(
-                        publicChannels = result.value
-                    )
-
-                is Failure -> FindChannelScreenState.Error(
+            val curr = state.value
+            if (curr !is FindChannelScreenState.Idle) return@launch
+            _state.emit(FindChannelScreenState.Loading)
+            when (val result = service.getChannels(offset, limit)) {
+                is Success -> _state.emit(FindChannelScreenState.Idle(_searchText.value, result.value))
+                is Failure -> _state.emit(FindChannelScreenState.Error(
                     ResponseError(
                         result.value.cause,
                         result.value.urlInfo
                     )
-                )
+                ))
             }
         }
+    }
+
+    fun updateSearchText(searchText: String) {
+       viewModelScope.launch {
+           val curr = state.value
+           if (curr !is FindChannelScreenState.Idle) return@launch
+           if (curr.searchChannelInput == searchText) return@launch
+           _searchText.emit(searchText)
+           _state.emit(curr.copy(searchChannelInput = searchText))
+       }
     }
 }
