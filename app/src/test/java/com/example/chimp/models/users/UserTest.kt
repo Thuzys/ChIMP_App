d3a4a3063a0ca9777d147bc2e90bb702fa4e70ee
Brgm37 <brgm37@gmail.com>
2024-12-18 20:04:42 +0000

    - missing token data class was added

--- app/src/test/java/com/example/chimp/models/users/UserTest.kt
+++ app/src/test/java/com/example/chimp/models/users/UserTest.kt
@@ -3,23 +3,31 @@ package com.example.chimp.models.users
 import org.junit.Test
 
 class UserTest {
+
+    private val token = Token("123")
+    private val emptyToken = Token("")
+    private val name = "Alice"
+    private val emptyName = ""
+    private val id = 1u
+    private val invalidId = 0u
+
     @Test
     fun create_a_valid_user() {
-        User(1u, "Alice", "123")
+        User(id, "Alice", token)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun create_an_user_with_invalid_id_throws_an_exception() {
-        User(0u, "Alice", "123")
+        User(invalidId, name, token)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun create_an_user_with_invalid_name_throws_an_exception() {
-        User(1u, "", "123")
+        User(id, emptyName, token)
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun create_an_user_with_invalid_token_throws_an_exception() {
-        User(1u, "Alice", "")
+        User(id, name, emptyToken)
     }
 }
