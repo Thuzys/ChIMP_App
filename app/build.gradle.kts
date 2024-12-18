cf44a6355e8ec98a646eb8bcacf69d1a740c0174
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-12-18 00:20:13 +0000

    build gradle update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.androidx.datastore)
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
