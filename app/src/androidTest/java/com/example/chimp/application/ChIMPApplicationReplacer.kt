f7a5e0899bdf010e5b24d6880d83263b92e00602
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-12-18 16:29:55 +0000

    Application replacer update.

--- app/src/androidTest/java/com/example/chimp/application/ChIMPApplicationReplacer.kt
+++ app/src/androidTest/java/com/example/chimp/application/ChIMPApplicationReplacer.kt
@@ -6,12 +6,15 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.chimp.infrastructure.UserInfoPreferencesRepository
 import com.example.chimp.models.repository.UserInfoRepository
+import com.example.chimp.screens.channels.model.channel.ChannelsServices
 import com.example.chimp.screens.findChannel.model.FindChannelService
 import com.example.chimp.screens.register.model.FormValidation
 import com.example.chimp.screens.register.model.RegisterService
 
 class ChIMPApplicationReplacer: Application(), DependenciesContainer {
     override val loginService: RegisterService
+        get() = TODO("Not yet implemented")
+    override val channelsService: ChannelsServices
         get() = TODO("Not yet implemented")
     override val findChannelService: FindChannelService
         get() = TODO("Not yet implemented")
