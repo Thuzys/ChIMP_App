9f13639dd5638d3c7d7c85538a1d9b43314bc858
Brgm37 <brgm37@gmail.com>
2024-12-20 02:04:52 +0000

    FindChannels test implementation. note: there is some missing view tests that will be added in the future

--- app/src/androidTest/java/com/example/chimp/screens/findChannel/screen/ChIMPFindChannelScreenKtTest.kt
+++ app/src/androidTest/java/com/example/chimp/screens/findChannel/screen/ChIMPFindChannelScreenKtTest.kt
@@ -1,5 +1,5 @@
 package com.example.chimp.screens.findChannel.screen
 
-import org.junit.jupiter.api.Assertions.*
+
 
 class ChIMPFindChannelScreenKtTest
--- app/src/androidTest/java/com/example/chimp/screens/findChannel/screen/view/ErrorViewKtTest.kt
+++ app/src/androidTest/java/com/example/chimp/screens/findChannel/screen/view/ErrorViewKtTest.kt
@@ -1,5 +1,3 @@
 package com.example.chimp.screens.findChannel.screen.view
 
-import org.junit.jupiter.api.Assertions.*
-
 class ErrorViewKtTest
