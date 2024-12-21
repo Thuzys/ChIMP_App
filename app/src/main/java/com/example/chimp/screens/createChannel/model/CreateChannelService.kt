cbff6d282691297be2b66d9922fa49fcc55b900c
mariana-moraes <A50560@alunos.isel.pt>
2024-12-21 18:08:39 +0000

    Create Channel Service improvement

--- app/src/main/java/com/example/chimp/screens/createChannel/model/CreateChannelService.kt
+++ app/src/main/java/com/example/chimp/screens/createChannel/model/CreateChannelService.kt
@@ -1,13 +1,8 @@
 package com.example.chimp.screens.createChannel.model
 
-import com.example.chimp.models.channel.AccessControl
 import com.example.chimp.models.channel.ChannelBasicInfo
-import com.example.chimp.models.channel.Visibility
 import com.example.chimp.models.either.Either
 import com.example.chimp.models.errors.ResponseError
-import kotlinx.coroutines.flow.Flow
-
-typealias FetchChannelsByNamesResult = Pair<Flow<List<ChannelBasicInfo>>, Flow<Boolean>>
 
 /**
  * Interface that defines the service used in CreateChannelViewModel.
@@ -16,24 +11,20 @@ interface CreateChannelService {
     /**
      * Fetch channels by their partial names.
      * @param channelName the partial name of the channel to find
-     * @return an [Either] with a [List] of [ChannelBasicInfo] if the channels were found,
+     * @return an [Either] with the [ChannelBasicInfo] if the channel was found,
      * or a [ResponseError] if it failed.
      */
-    suspend fun fetchChannelsByNames(
+    suspend fun fetchChannelByNames(
         channelName: String
-    ): Either<ResponseError,List<ChannelBasicInfo>>
+    ): Either<ResponseError,ChannelBasicInfo>
 
     /**
      * Create a channel.
-     * @param channelName the name of the channel to create
-     * @param visibility the visibility of the channel
-     * @param accessControl the access control of the channel
+     * @param channelInput the input to create the channel
      * @return an [Either] with [Unit] if the channel was created,
      * or a [ResponseError] if it failed.
      */
     suspend fun createChannel(
-        channelName: String,
-        visibility: Visibility,
-        accessControl: AccessControl
+        channelInput: ChannelInput
     ): Either<ResponseError, Unit>
 }
