a69f079b48cba543d1b925ef42be2747cac0a642
mariana-moraes <A50560@alunos.isel.pt>
2024-12-21 21:25:36 +0000

    Create Channel screen starte improvement

--- app/src/main/java/com/example/chimp/screens/createChannel/viewModel/state/CreateChannelScreenState.kt
+++ app/src/main/java/com/example/chimp/screens/createChannel/viewModel/state/CreateChannelScreenState.kt
@@ -3,21 +3,31 @@ package com.example.chimp.screens.createChannel.viewModel.state
 import com.example.chimp.models.errors.ResponseError
 
 interface CreateChannelScreenState {
+
     data object BackToRegistration : CreateChannelScreenState
+
     data class Error(
         val error: ResponseError,
         val goBack: CreateChannelScreenState
     ) : CreateChannelScreenState
-    data object Successful : CreateChannelScreenState
-    data class Editing(
-        val channelName: String,
-    ) : CreateChannelScreenState
-    data class Validated(
-        val channelName: String,
-    ) : CreateChannelScreenState
-    data object NotValidated : CreateChannelScreenState
-    data class Submit(
-        val channelName: String,
-    ) : CreateChannelScreenState
 
+    data object Successful : CreateChannelScreenState
+
+    data class Editing(
+        override val channelName: String,
+    ) : EditingState
+
+    data class Validated(
+        override val channelName: String,
+    ) : EditingState
+
+    data class NotValidated(
+        override val channelName: String
+    ) : EditingState
+
+    data object Submit : CreateChannelScreenState
+
+    interface EditingState : CreateChannelScreenState{
+        val channelName: String
+    }
 }
