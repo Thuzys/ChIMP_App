0a3d9c2a303617401b5eb02e7d87a3df2104a15f
mariana-moraes <A50560@alunos.isel.pt>
2024-12-21 15:27:10 +0000

    Create Channel Screen improvement

--- app/src/main/java/com/example/chimp/screens/createChannel/screen/ChIMPCreateChannelScreen.kt
+++ app/src/main/java/com/example/chimp/screens/createChannel/screen/ChIMPCreateChannelScreen.kt
@@ -1,8 +1,21 @@
 package com.example.chimp.screens.createChannel.screen
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import com.example.chimp.screens.createChannel.viewModel.CreateChannelViewModel
+import com.example.chimp.screens.ui.composable.MenuBottomBar
 
+
+const val CREATE_CHANNEL_SCREEN_TAG = "CreateChannelScreen"
+
+@Composable
 fun ChIMPCreateChannelScreen(
     modifier: Modifier = Modifier,
     viewModel: CreateChannelViewModel,
@@ -12,4 +25,38 @@ fun ChIMPCreateChannelScreen(
     onRegisterNavigate: () -> Unit
 ) {
 
+
+//    Column(
+//        modifier = modifier
+//    ) {
+//        val curr = viewModel.state.collectAsState().value
+//        Log.i(CREATE_CHANNEL_SCREEN_TAG, "State: ${curr::class.simpleName}")
+//        FindChannelScreenAux(
+//            bottomBar = {
+//                MenuBottomBar(
+//                    findChannelIsEnable = false,
+//                    onMenuClick = onChatsNavigate,
+//                    aboutClick = onAboutNavigate
+//                )
+//            }
+//        ) { modifier ->
+//
+//            when(curr) {
+//
+//                }
+//            }
+//        }
+//    }
+}
+
+@Composable
+private fun FindChannelScreenAux(
+    bottomBar: @Composable () -> Unit = {},
+    content: @Composable (Modifier) -> Unit
+) {
+    Scaffold(
+        bottomBar = bottomBar
+    ) { innerPadding ->
+        content(Modifier.padding(innerPadding))
+    }
 }
